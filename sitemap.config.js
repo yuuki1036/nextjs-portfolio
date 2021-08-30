3cbbc4d3b811f50550ed9fa2556a48eb33d51ca8
yuuki1036 <yuuki1036@gmail.com>
2021-08-30 18:33:48 +0900

    fix sitemap

--- sitemap.config.js
+++ sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: "https://www.yuuki1036-portfolio.ml/",
+  siteUrl: "https://www.yuuki1036-portfolio.ml",
   generateRobotsTxt: true,
   sitemapSize: 7000,
   outDir: "./public",
